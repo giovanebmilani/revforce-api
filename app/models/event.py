bbcda4af96fad5dc6c3744aaf248ab3dbf1502f4
Erick <erickcarpesmmc@gmail.com>
2025-04-06 12:58:13 -0300

    models de chart_source e event criados

---
+++ app/models/event.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
+
+from app.config.database import Base
+
+class Event(Base):
+    __tablename__ = "events"
+    id: Mapped[str] = mapped_column(primary_key=True)
+    chart_id: Mapped[str] = mapped_column(ForeignKey('charts.id'))
+    name: Mapped[str]
+    description: Mapped[str]
+    date: Mapped[datetime]
+    color: Mapped[str]
