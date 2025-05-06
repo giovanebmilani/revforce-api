f03cbaad7c3a564f4e0ef571e426c6e94cade915
Pablo Montiel <pablopmontiel98@gmail.com>
2025-05-06 00:18:22 -0300

    add característica de position para persistir a mudança, ajuste no init para poder persistir os dados

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,9 @@ from .todos import Todos
 from .account import Account
 from .period import Period
 from .account_config import AccountConfig
+
+from .chart import Chart
+from .chart_source import ChartSource
+from .ad import Ad
+from .ad_metric import AdMetric
+from .campaign import Campaign
