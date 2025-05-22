e52976c44a40b4d9728797d057c1ca9188f6c0ac
Pércio Reinert <percioreinert@gmail.com>
2025-05-21 21:38:07 -0300

    feat: add new database migration

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,9 @@
-from .todos import Todos
 from .account import Account
-from .period import Period
-from .account_config import AccountConfig
+from .account_config import AccountConfig, AccountType
+from .ad import Ad
+from .ad_metric import AdMetric, DeviceType
+from .campaign import Campaign
+from .chart import Chart, ChartMetric, ChartSegment, ChartType
+from .chart_source import ChartSource, SourceTable
+from .event import Event
+from .period import Period, PeriodType
