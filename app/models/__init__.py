9a220f44991eed4c0bf3db82bdb5fb3d8b621487
Bruno Panizzi <brunopanizziq@gmail.com>
2025-05-27 18:56:59 -0300

    add imports to __init__.py to generate migrations

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,10 @@
-from .todos import Todos
+# from .todos import Todos
 from .account import Account
+from .account_config import AccountConfig, AccountType
+from .ad import Ad
+from .ad_metric import AdMetric, DeviceType
+from .campaign import Campaign
+from .chart import Chart, ChartType
+from .chart_source import ChartSource, ChartMetric, SourceTable
+from .event import Event
 from .period import Period
-from .account_config import AccountConfig
