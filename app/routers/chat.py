471c227610a9bc7fa86c67939d4d22fe61457eb2
Pércio Reinert <percioreinert@gmail.com>
2025-06-02 11:30:59 -0300

    feat: add chat with assistant

--- app/routers/chat.py
+++ app/routers/chat.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from starlette import status
 
-from app.schemas.chat import ChatRequest, ChatResponse
+from app.schemas.chat import ChatRequest, ChatResponse, History
 from app.services.chat import ChatService
 
 router = APIRouter(
@@ -13,3 +13,7 @@ router = APIRouter(
 @router.post("/", status_code=status.HTTP_200_OK, response_model=ChatResponse)
 async def chat(chat_data: ChatRequest, service: ChatService = Depends(ChatService.get_service)):
     return await service.chat(chat_data)
+
+@router.post("/assistant", response_model=ChatResponse)
+async def chat_assistant(chat_data: ChatRequest, service: ChatService = Depends(ChatService.get_service)):
+    return await service.chat_assistant(chat_data)
